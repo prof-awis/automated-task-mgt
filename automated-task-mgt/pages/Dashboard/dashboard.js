a9181b56181acb1364d2f61ae863f5fcdf311be4
prof-awis <awinoemanuel2000@gmail.com>
2023-03-03 18:38:49 +0300

    Giving the menu bar functionality

--- automated-task-mgt/pages/Dashboard/dashboard.js
+++ automated-task-mgt/pages/Dashboard/dashboard.js
@@ -2,9 +2,7 @@
 
 let sidebar = document.querySelector(".sidebar");
 let menubar = document.querySelector(".sidebarBtn");
-sidebarBtn.onclick = function () {
+
+menubar.onclick = function () {
   sidebar.classList.toggle("active");
-  if (sidebar.classList.contains("active")) {
-    sidebarBtn.classList.replace("fa-bars", "fa-bars-alt-right");
-  } else sidebarBtn.classList.replace("fa-bars-alt-right", "fa-bars");
 };
